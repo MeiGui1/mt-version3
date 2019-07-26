448a5beda6db3cf6ef089676ff242711ec80fb92
MeiGui1 <alice_truong95@hotmail.com>
2019-07-26 02:05:55 +0200

    add all 3d models

--- app/src/main/java/com/example/frontend/Fragments/ModelsFragment.java
+++ app/src/main/java/com/example/frontend/Fragments/ModelsFragment.java
@@ -1,15 +1,13 @@
 package com.example.frontend.Fragments;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -22,7 +20,17 @@ public class ModelsFragment extends Fragment {
     private WebView modelView;
     private String currentUrl;
     private ImageView ivKauen;
+    private ImageView ivBruxismus;
+    private ImageView ivNervensystem;
+    private ImageView ivNeuropathie;
+    private ImageView ivMyelinscheide;
+    private ImageView ivMigraene;
     private ImageView ivQuerschnitt;
+    private ImageView ivSensitiv;
+    private ImageView ivErosion;
+    private ImageView ivHoehlen;
+    private int currentModel;
+    private boolean firstModelSelected = false;
 
     private View.OnClickListener onClickListener;
 
@@ -37,12 +45,66 @@ public class ModelsFragment extends Fragment {
             public void onClick(View view) {
                 switch (view.getId()) {
                     case R.id.iv_kauen:
+                        deselectImageView(currentModel);
+                        currentModel = R.id.iv_kauen;
+                        selectImageView(currentModel);
                         currentUrl = "https://human.biodigital.com/widget/?be=2vwf&ui-annotations=true&ui-info=true&ui-zoom=true&ui-share=true&uaid=4Aty9&lang=de";
                         break;
-                    case R.id.iv_querschnitt:
-                        currentUrl = "https://human.biodigital.com/widget/?be=31qc&ui-tools=true&ui-dissect=true&ui-isolate=true&ui-xray=true&ui-cross-section=false&ui-annotations=true&ui-info=true&ui-zoom=true&ui-share=true&uaid=4AuuL&lang=de";
+                    case R.id.iv_bruxismus:
+                        deselectImageView(currentModel);
+                        currentModel = R.id.iv_bruxismus;
+                        selectImageView(currentModel);
+                        currentUrl = "https://human.biodigital.com/widget/?be=31sB&ui-tools=true&ui-dissect=true&ui-isolate=true&ui-xray=true&ui-cross-section=false&ui-annotations=true&ui-info=true&ui-zoom=true&ui-share=true&uaid=4B0RT&lang=de";
                         break;
-                }
+                    case R.id.iv_nervensystem:
+                        deselectImageView(currentModel);
+                        currentModel = R.id.iv_nervensystem;
+                        selectImageView(currentModel);
+                        currentUrl = "https://human.biodigital.com/widget/?be=31sG&ui-tools=true&ui-dissect=true&ui-isolate=true&ui-xray=true&ui-cross-section=false&ui-annotations=true&ui-info=true&ui-zoom=true&ui-share=true&uaid=4B15y&lang=de";
+                        break;
+                    case R.id.iv_neuropathie:
+                        deselectImageView(currentModel);
+                        currentModel = R.id.iv_neuropathie;
+                        selectImageView(currentModel);
+                        currentUrl = "https://human.biodigital.com/widget/?be=31sN&ui-tools=true&ui-dissect=true&ui-isolate=true&ui-xray=true&ui-cross-section=false&ui-annotations=true&ui-info=true&ui-zoom=true&ui-share=true&uaid=4B18A&lang=de";
+                        break;
+                    case R.id.iv_myelinscheide:
+                        deselectImageView(currentModel);
+                        currentModel = R.id.iv_myelinscheide;
+                        selectImageView(currentModel);
+                        currentUrl = "https://human.biodigital.com/widget/?be=31sD&ui-tools=true&ui-dissect=true&ui-isolate=true&ui-xray=true&ui-cross-section=false&ui-annotations=true&ui-info=true&ui-zoom=true&ui-share=true&uaid=4B1iH&lang=de";
+                        break;
+                    case R.id.iv_migraene:
+                        deselectImageView(currentModel);
+                        currentModel = R.id.iv_migraene;
+                        selectImageView(currentModel);
+                        currentUrl = "https://human.biodigital.com/widget/?be=31sL&ui-tools=true&ui-dissect=true&ui-isolate=true&ui-xray=true&ui-cross-section=false&ui-annotations=true&ui-info=true&ui-zoom=true&ui-share=true&uaid=4B13t&lang=de";
+                        break;
+                    case R.id.iv_querschnitt:
+                        deselectImageView(currentModel);
+                        currentModel = R.id.iv_querschnitt;
+                        selectImageView(currentModel);
+                        currentUrl = "https://human.biodigital.com/widget/?be=31qc&ui-tools=true&ui-dissect=true&ui-isolate=true&ui-xray=true&ui-cross-section=false&ui-annotations=true&ui-info=true&ui-zoom=true&ui-share=true&uaid=4B1cA&lang=de";
+                        break;
+                    case R.id.iv_sensitiv:
+                        deselectImageView(currentModel);
+                        currentModel = R.id.iv_sensitiv;
+                        selectImageView(currentModel);
+                        currentUrl = "https://human.biodigital.com/widget/?be=31sV&ui-tools=true&ui-dissect=true&ui-isolate=true&ui-xray=true&ui-cross-section=false&ui-annotations=true&ui-info=true&ui-zoom=true&ui-share=true&uaid=4B1Yj&lang=de";
+                        break;
+                    case R.id.iv_erosion:
+                        deselectImageView(currentModel);
+                        currentModel = R.id.iv_erosion;
+                        selectImageView(currentModel);
+                        currentUrl = "https://human.biodigital.com/widget/?be=31sR&ui-tools=true&ui-dissect=true&ui-isolate=true&ui-xray=true&ui-cross-section=false&ui-annotations=true&ui-info=true&ui-zoom=true&ui-share=true&uaid=4B1U2&lang=de";
+                        break;
+                    case R.id.iv_hoehlen:
+                        deselectImageView(currentModel);
+                        currentModel = R.id.iv_hoehlen;
+                        selectImageView(currentModel);
+                        currentUrl = "https://human.biodigital.com/widget/?be=31sW&ui-tools=true&ui-dissect=true&ui-isolate=true&ui-xray=true&ui-cross-section=false&ui-annotations=true&ui-info=true&ui-zoom=true&ui-share=true&uaid=4B1dV&lang=de";
+                        break;
+                    }
                 modelView.loadUrl(currentUrl);
                 // Enable Javascript
                 WebSettings webSettings = modelView.getSettings();
@@ -58,7 +120,40 @@ public class ModelsFragment extends Fragment {
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         ivKauen = view.findViewById(R.id.iv_kauen);
         ivKauen.setOnClickListener(onClickListener);
+        ivBruxismus = view.findViewById(R.id.iv_bruxismus);
+        ivBruxismus.setOnClickListener(onClickListener);
+
+        ivNervensystem = view.findViewById(R.id.iv_nervensystem);
+        ivNervensystem.setOnClickListener(onClickListener);
+        ivNeuropathie = view.findViewById(R.id.iv_neuropathie);
+        ivNeuropathie.setOnClickListener(onClickListener);
+        ivMyelinscheide = view.findViewById(R.id.iv_myelinscheide);
+        ivMyelinscheide.setOnClickListener(onClickListener);
+        ivMigraene = view.findViewById(R.id.iv_migraene);
+        ivMigraene.setOnClickListener(onClickListener);
+
         ivQuerschnitt = view.findViewById(R.id.iv_querschnitt);
         ivQuerschnitt.setOnClickListener(onClickListener);
+        ivSensitiv = view.findViewById(R.id.iv_sensitiv);
+        ivSensitiv.setOnClickListener(onClickListener);
+        ivErosion = view.findViewById(R.id.iv_erosion);
+        ivErosion.setOnClickListener(onClickListener);
+        ivHoehlen = view.findViewById(R.id.iv_hoehlen);
+        ivHoehlen.setOnClickListener(onClickListener);
+    }
+
+    public void deselectImageView(int imageViewId){
+        if(firstModelSelected){
+            ImageView imageView = (ImageView)getView().findViewById(imageViewId);
+            imageView.setSelected(false);
+            imageView.setBackgroundColor(0);
+        }
+    }
+
+    public void selectImageView(int imageViewId){
+        firstModelSelected = true;
+        ImageView imageView = (ImageView)getView().findViewById(imageViewId);
+        imageView.setSelected(true);
+        imageView.setBackgroundColor(getResources().getColor(R.color.colorBlue));
     }
 }
