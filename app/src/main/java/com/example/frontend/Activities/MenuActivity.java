42b3ba986385bf7b68555fca22716841b046a093
MeiGui1 <alice_truong95@hotmail.com>
2019-08-14 03:07:40 +0200

    add download pdf library and inital test

--- app/src/main/java/com/example/frontend/Activities/MenuActivity.java
+++ app/src/main/java/com/example/frontend/Activities/MenuActivity.java
@@ -1,8 +1,13 @@
 package com.example.frontend.Activities;
 
+import android.Manifest;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.Resources;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,7 +17,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewTreeObserver;
@@ -28,15 +32,21 @@ import com.example.frontend.Fragments.NotesFragment;
 import com.example.frontend.Fragments.OverviewFragment;
 import com.example.frontend.Fragments.PainFragment;
 import com.example.frontend.Fragments.PsychosocialFragment;
-import com.example.frontend.Fragments.RecognizerFragment;
 import com.example.frontend.Fragments.TranslatorFragment;
 import com.example.frontend.Globals;
 import com.example.frontend.Models.Patient;
 import com.example.frontend.R;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
-import java.util.Locale;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 
 public class MenuActivity extends AppCompatActivity {
+    private static final int STORAGE_CODE = 1000;
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
@@ -167,10 +177,26 @@ public class MenuActivity extends AppCompatActivity {
                         exercisesFrag.setArguments(bundlePatientId);
                         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, exercisesFrag).commit();
                         break;
-                    case R.id.nav_person:
+                    case R.id.nav_pdf:
+                        /*
                         setTitle("Patienten");
                         Intent intent = new Intent(MenuActivity.this, PatientSelectionActivity.class);
-                        startActivity(intent);
+                        startActivity(intent);*/
+                        //handle runtime permission for devices with marshmallow and above
+                        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+                            //system OS >= Marshmallow (6.0), check if permission is enabled or not
+                            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                                //permission was not granted, request it
+                                String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                                requestPermissions(permissions, STORAGE_CODE);
+                            }
+                            else{
+                                //permission already granted
+                                savePdf();
+                            }
+                        }else{
+                            //system OS < Marshmallow no required to check runtime permission
+                        }
                         break;
                 }
 
@@ -179,6 +205,56 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void savePdf() {
+        Document mDoc = new Document();
+        //pdf file name
+        String fileName = "Summary - " + patient.getShortname();
+        //pdf file path
+        String filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" +fileName + ".pdf";
+        try {
+            PdfWriter.getInstance(mDoc, new FileOutputStream(filePath));
+            //open the document for writing
+            mDoc.open();
+            mDoc.addAuthor("Alice Truong");
+            //add paragraph to the document
+            mDoc.add(new Paragraph("Das ist ein Beispiel."));
+            mDoc.addTitle("Titel");
+            mDoc.addCreationDate();
+            String imFile = "C:/itextExamples/logo.jpg";
+
+
+            Drawable d = getResources().getDrawable(R.drawable.teeth); // the drawable (Captain Obvious, to the rescue!!!)
+            Bitmap bitmap = ((BitmapDrawable)d).getBitmap();
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            byte[] bitmapdata = stream.toByteArray();
+
+            Image image = Image.getInstance(bitmapdata);
+            mDoc.add(image);
+
+            //close the document
+            mDoc.close();
+            Toast.makeText(this,fileName + ".pdf " +R.string.saved, Toast.LENGTH_SHORT).show();
+
+        }catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case STORAGE_CODE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //permission was granted from popup
+                    savePdf();
+                }else{
+                    //permission was denied from popup, show error message
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+        }
     }
 
     private void displayMessage(String message) {
@@ -274,8 +350,6 @@ public class MenuActivity extends AppCompatActivity {
         }
 
     }
-
-
 
 
     public void navigateBackToPatients(View view) {
